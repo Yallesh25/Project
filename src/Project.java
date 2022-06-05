93259e8e86e48d93beeca354b773e1c6359d4ac6
Yallesh25 <yallesh42@gmail.com>
2022-06-05 13:22:16 +0530

    first commit

--- src/Project.java
+++ src/Project.java
@@ -1,6 +1,21 @@
 
 public class Project {
+	static int countDigits(int number) {
+		if(number>=1 && number <=9)
+			return 1;
+		else if (number>=10 && number <=99)
+			return 2;
+		else if (number>=100 && number <=999)
+			return 3;
+		else
+			return -1;
+	}
 	public static void main(String args[]) {
 		System.out.print("Hello");
+		int x=564;
+		if(countDigits(x)!=-1);
+			System.out.print(countDigits(x)+"digits");
+		else;
+			System.out.print( "Multiple Digits");
 	}
 }
